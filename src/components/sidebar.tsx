daaaff1e0245ab40df1c2c8ed48a2846aea94bb5
rdanang-dev <rdanang.dev@gmail.com>
2024-09-07 23:36:47 +0700

    add logo

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,6 +2,8 @@ import { ChevronRight, LayoutDashboard, User } from 'lucide-react';
 import React from 'react';
 import { Button } from './ui/button';
 import { Nav } from './ui/nav';
+import { NavLink } from 'react-router-dom';
+import logoUrl from '/home/art3mis/projects/prelek-frontend-from/src/images/prelek-logo.png';
 
 export interface SidebarProps {}
 
@@ -13,39 +15,45 @@ export default function Sidebar({}: SidebarProps) {
   }
 
   return (
-    <div className="relative min-w-[80px] border-r px-3 pb-10 pt-24">
-      <div className="absolute right-[-20px] top-7">
-        <Button
-          variant="secondary"
-          className="rounded-full p-2"
-          onClick={toggleCollapsed}
-        >
-          <ChevronRight
-            className={`transform transition-transform duration-300 ${
-              isCollapsed ? 'rotate-0' : 'rotate-180'
-            }`}
-          />
-        </Button>
+    <div className="relative ">
+      <NavLink to="/" className="block">
+        <img className="absolute w-full md:mb-8" src={logoUrl} alt="Logo" />
+      </NavLink>
+      <div className="relative min-w-[90px] border-r px-3 pb-10 pt-24">
+        <div className="absolute right-[-20px] top-7">
+          <Button
+            variant="secondary"
+            className="rounded-full p-2"
+            onClick={toggleCollapsed}
+          >
+            <ChevronRight
+              className={`transform transition-transform duration-300 ${
+                isCollapsed ? 'rotate-0' : 'rotate-180'
+              }`}
+            />
+          </Button>
+        </div>
+
+        <Nav
+          isCollapsed={isCollapsed}
+          links={[
+            {
+              title: 'Dashboard',
+              label: '128',
+              icon: LayoutDashboard,
+              href: '/',
+              variant: 'default',
+            },
+            {
+              title: 'Users',
+              label: '9',
+              icon: User,
+              href: '/users/list',
+              variant: 'ghost',
+            },
+          ]}
+        />
       </div>
-      <Nav
-        isCollapsed={isCollapsed}
-        links={[
-          {
-            title: 'Dashboard',
-            label: '128',
-            icon: LayoutDashboard,
-            href: '/',
-            variant: 'default',
-          },
-          {
-            title: 'Users',
-            label: '9',
-            icon: User,
-            href: '/users/list',
-            variant: 'ghost',
-          },
-        ]}
-      />
     </div>
   );
 }
